00ada3b98b41dfff8ef2944ac0ea3942159617ae
Craig Citro <craig@citro.net>
2023-07-10 14:07:29 -0500

    Omit duration if no start date was specified

--- components/duration.js
+++ components/duration.js
@@ -2,5 +2,8 @@ import html from '../utils/html.js'
 import Date from './date.js'
 
 export default function Duration(startDate, endDate) {
+  if (!startDate) {
+    return "";
+  }
   return html`${Date(startDate)} – ${endDate ? Date(endDate) : 'Present'}`
 }
